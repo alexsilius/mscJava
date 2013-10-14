9fd52e527a8b1a1c49763e2d62358f5ddb758b0e
Alex <alex.silius.dev@gmail.com>
2013-10-14 20:39:03 +0100

    Changes to task 2. Added case system

--- tasks2.groovy
+++ tasks2.groovy
@@ -1,48 +1,65 @@
+String task
 
+print "Please input task number:"
+task = System.console().readLine()
+num = Integer.parseInt(task)
 
+ 
+switch(num){
 
-	println("Please type 2 numbers in order to perform calculation");
-	print("number 1:");
-	int x = Integer.parseInt(System.console().readLine());
-	print("number 2:");
-	int y = Integer.parseInt(System.console().readLine());
-	print("Please type a simbol (-, +, *, /) for the following calculation: ")
-	String action = System.console().readLine(); 
-	int summ = 0;
-	
-	if(action == "+"){
-		 summ = Calculator.addition(x, y);
-	} 
-	else if(action == "-"){
-		summ = Calculator.subtraction(x, y);	
-	}
-	else if(action == "*"){
-		summ = Calculator.multiplication(x, y);	
-	}
-	else if(action == "/"){
-		summ = Calculator.division(x, y);
-	}
-	else{println("Incorrect simbol!");}
-	
-	
-	class Calculator {
-	
-		public static int addition(int x,int y) {
-		return x+y;
+	case 1:
+
+		println("Please type 2 numbers in order to perform calculation");
+		print("number 1:");
+		int x = Integer.parseInt(System.console().readLine());
+		print("number 2:");
+		int y = Integer.parseInt(System.console().readLine());
+		print("Please type a simbol (-, +, *, /) for the following calculation: ")
+		String action = System.console().readLine(); 
+		int summ = 0;
+		
+		if(action == "+"){
+			 summ = Calculator.addition(x, y);
+		} 
+		else if(action == "-"){
+			summ = Calculator.subtraction(x, y);	
+		}
+		else if(action == "*"){
+			summ = Calculator.multiplication(x, y);	
+		}
+		else if(action == "/"){
+			summ = Calculator.division(x, y);
+		}
+		else{println("Incorrect simbol!");}
+		
+		
+		class Calculator {
+		
+			public static int addition(int x,int y) {
+			return x+y;
+			}
+			
+			public static int subtraction(int x,int y) {
+			return x-y;
+			}
+			
+			public static int multiplication(int x,int y) {
+			return x*y;
+			}
+			
+			public static int division(int x,int y) {
+			return x/y;
+			}
+			
 		}
 		
-		public static int subtraction(int x,int y) {
-		return x-y;
-		}
-		
-		public static int multiplication(int x,int y) {
-		return x*y;
-		}
-		
-		public static int division(int x,int y) {
-		return x/y;
-		}
-		
-	}
+		print("Result: "+summ);
 	
-	print("Result: "+summ);
+	break;
+	
+	default:
+	
+		println "This task is not completed or not on the list" 
+		
+    break
+	}
